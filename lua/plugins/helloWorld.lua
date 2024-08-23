228947ef5d4c22c10ea37181143cc6f0c95eaabd
whitney <whitney.server@gmail.com>
2024-08-23 16:57:12 -0700

    Remove relative numberlines and hello world buffer enter trigger

--- lua/plugins/helloWorld.lua
+++ lua/plugins/helloWorld.lua
@@ -8,11 +8,3 @@ end
 -- Create a command ":SayHello" that runs the say_hello function
 vim.api.nvim_create_user_command('HelloWorld', hello_world, {})
 
--- Optionally, set up an autocmd that says hello when entering a buffer
-vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = "*",
-  callback = function()
-    vim.notify("Welcome to Jake's Neovim!")
-  end,
-})
-
