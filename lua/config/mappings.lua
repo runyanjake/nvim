0442b9e1f408bf7c01f0ab0554038ebbb9991201
whitney <whitney.server@gmail.com>
2024-08-28 18:59:08 -0700

    Set Leader key to Space to match runyanjake/lazyvim

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,6 +1,5 @@
 -- set <LEADER> key for nvim (effectively layer key for shortcuts in nvim)
-vim.g.mapleader = ","
+vim.g.mapleader = " "
 
 -- map leader+y to copy to system clipboard in normal mode
 vim.keymap.set({ "n", "v" }, "<Leader>y", '"+y', { noremap = true, silent = true })
-
